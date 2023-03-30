07ee4088210ec7fa6b6e28e686d05173a1ba9bfa
HaliyK2 <113514104+HaliyK2@users.noreply.github.com>
2023-03-30 17:10:12 +0800

    Create t9.c
    将一个整型变量的值赋给一个布尔型变量，再将这个布尔型变量的值赋给一个整型变量，得到的值是多少

---
+++ t9.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  //输入格式
+  //一个整型范围内的整数，即初始时整型变量的值。
+//输出格式
+//一个整数，经过上述过程后得到的结果
+ //输入 3  
+  //输出 1
+    int a;
+    bool b;
+    scanf("%d",&a);
+    b=(bool)a;
+    printf("%d",(int)b);
+return 0;
+}
